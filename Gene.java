4802174936f4c6e4cf2d902fa81216226ed835fd
Matheus Alves <mr634580@gmail.com>
2019-06-18 15:59:30 -0300

    INF008

---
+++ Gene.java
@@ -0,0 +1,55 @@
+package Application01;
+
+public class Gene {
+    private int valor;
+    
+    public Gene(int valor) {
+        // TODO Auto-generated constructor stub
+        this.setValor(valor);
+    }
+    
+    public Gene() {
+        // TODO Auto-generated constructor stub
+    	//this.setValor(1);
+        this(1);
+    }
+    
+    public Gene(Gene gene) {
+        // TODO Auto-generated constructor stub
+        this(gene.getValor());
+    }
+
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        if(valor > 0)
+            this.valor = valor;
+        else
+            this.valor = 1;
+    }
+    
+    public boolean isEquals(Gene gene) {
+        return this.getValor() == gene.getValor(); 
+    }
+    
+    public Gene clonador() {
+        Gene geneclone = new Gene();
+        geneclone.valor = this.valor;
+        return geneclone;
+    }
+    
+    public String toString(){
+        return " [ "+ getValor() +" ] ";
+    }
+    
+    public int getAfinidade(int valorProteina){
+        return Math.abs(this.getValor() - valorProteina);
+    }
+    
+    public int getAfinidade(Gene gene){
+        return this.getAfinidade(gene.getValor());
+    }
+}
