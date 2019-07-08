62073c57a5d97ec155a79c32adcf79e9f0dda5d0
Matheus Alves <mr634580@gmail.com>
2019-07-08 15:22:36 -0300

    Avaliação 2018.2

---
+++ av-2018-2/Verbete.java
@@ -0,0 +1,68 @@
+public class Verbete{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private String palavra;
+    //private int tamanhoListaSinonimos;
+    private String classeGramatical;
+    private String descricao;
+    private String[] listaSinonimos;
+
+    public Verbete(String palavra, String gramClass, String descricao){
+       this.palavra = palavra;
+       this.classeGramatical = gramClass;
+       this.descricao = descricao;
+    }
+    
+    public Verbete(String palavra, String gramClass, String descricao, int tamanho){
+       this.palavra = palavra;
+       this.classeGramatical = gramClass;
+       this.descricao = descricao;
+       //new String[tamanho];
+    }
+
+    public String getPalavra(){
+        // ponha seu código aqui
+        return this.palavra;
+    }
+       
+    public String getclasseGramatical(){
+        // ponha seu código aqui
+        return this.classeGramatical;
+    }
+       
+    public String getDescricao(){
+        // ponha seu código aqui
+        return this.descricao;
+    }
+    
+    public int comparaString(String s1){
+        int comparaString;
+        if(s1 == getPalavra())
+            comparaString = 1;
+        else
+            comparaString = 0;
+            
+        return comparaString;
+    }
+    
+    public int equalsVerbetes(Verbete v1){
+        int verificaVerbetes;
+        if((this.getPalavra() == v1.getPalavra()) 
+        && (this.getclasseGramatical() == v1.getclasseGramatical()))
+            verificaVerbetes = 1;
+         else 
+            verificaVerbetes = 0;
+        
+        return verificaVerbetes;
+    }
+    
+    public void adicionaSinonimo(String sinonimo){
+        for(int i=0; i<listaSinonimos.length; i++){
+            if(sinonimo.equals(listaSinonimos))
+                return;
+            else{
+                listaSinonimos[i+1] = sinonimo;
+            }
+        }
+    }
+    
+}
