af1e9838890fdbcfb8c2bdfa40710ace4682cd53
Matheus Rodrigues <matheus.rodrigues@ifba.edu.br>
2019-11-21 17:00:23 -0300

    construtor em enumeradores

--- prova20191/src/prova/model/CampoHarmonico.java
+++ prova20191/src/prova/model/CampoHarmonico.java
@@ -1,5 +1,11 @@
 package prova.model;
 
 public enum CampoHarmonico {
-	 C, D, E, F, G, A, B;
+	 C(1), D(2), E(3), F(4), G(5), A(6), B(7);
+	
+	private int valor;
+	
+	CampoHarmonico(int valor){
+		this.valor = valor;
+	}
 }
