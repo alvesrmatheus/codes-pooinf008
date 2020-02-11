fe46572da70569f3f31dc7bf2f85d65aade1f63b
Matheus Rodrigues <44441198+mtrs8@users.noreply.github.com>
2020-02-10 21:22:33 -0300

    Initial commit

---
+++ Avaliacao ll 2018.1/Coluna.java
@@ -0,0 +1,5 @@
+public class Coluna{
+    private String objeto_id;
+    private String nome;
+    private boolean estado;
+}
---
+++ Avaliacao ll 2018.1/Tabela.java
@@ -0,0 +1,5 @@
+public class Tabela{
+    private String objeto_id;
+    private String nome;
+    private Coluna[] colunas;
+}
